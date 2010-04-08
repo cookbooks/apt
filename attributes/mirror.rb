7a18fc0ba547eab82a0cd624ed9d60f9c3d1d501
Joshua Sierles <jsierles@gmail.com>
2010-04-08 15:22:47 +0200

    tweak some attributes for 0.8. update netboot recipe.

--- attributes/mirror.rb
+++ attributes/mirror.rb
@@ -1,12 +1,7 @@
-apt Mash.new
-apt[:mirror] = Mash.new
-apt[:mirror][:base_path] = "/u/mirrors/apt"
+default.apt[:mirror][:base_path] = "/u/mirrors/apt"
 
-apache Mash.new unless attribute?("apache")
-apache[:sites] = Mash.new unless apache.has_key?("sites")
-apache[:sites][:dist] = Mash.new
-apache[:sites][:dist][:server_name] = "dist.#{domain}"
-apache[:sites][:dist][:server_alias] = "dist"
-apache[:sites][:dist][:document_root] = "/u/mirrors/dist"
-apache[:sites][:dist][:error_log] = "/var/log/apache2/dist_error.log"
+default.apache[:sites][:dist][:server_name] = "dist.#{domain}"
+default.apache[:sites][:dist][:server_alias] = "dist"
+default.apache[:sites][:dist][:document_root] = "/u/mirrors/dist"
+default.apache[:sites][:dist][:error_log] = "/var/log/apache2/dist_error.log"
 
