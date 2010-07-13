9a5b1e8ef9229dc5d62c67d7d439087c74585ea0
Joshua Sierles <jsierles@gmail.com>
2010-07-13 17:46:52 +0200

    update to chef 0.9. Prefer nginx/unicorn over passenger. Too many other changes to mention here.

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,13 +1,3 @@
-
-if @node[:datacenter]
-  remote_file "/etc/apt/sources.list" do
-    source "sources.list.#{@node[:datacenter]}"
-    owner "root"
-    group "root"
-    mode 00644
-  end
-end
-
 execute "apt-get-update" do
   command "apt-get update"
 end
--- recipes/packages.rb
+++ recipes/packages.rb
@@ -3,7 +3,7 @@ require_recipe 'apt'
 
 if node[:packages]
   node[:packages].each do |group, packages|
-    Chef::Log.info "Installing packages for package group, #{group}"
+    Chef::Log.debug "Installing packages for package group, #{group}"
     packages.each do |name, options|
       package name do
         options.each { |opt, arg| send(opt.to_sym, arg) }
